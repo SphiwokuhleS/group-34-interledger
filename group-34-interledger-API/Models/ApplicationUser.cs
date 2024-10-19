b13b515bfe842f23c239d02be12a75562f7413d8
Byron Thompson <bthompso@student.wethinkcode.co.za>
2024-10-19 17:41:05 +0200

    Working api

--- group-34-interledger-API/Models/ApplicationUser.cs
+++ group-34-interledger-API/Models/ApplicationUser.cs
@@ -1,8 +1,11 @@
-﻿namespace group_34_interledger_API.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace group_34_interledger_API.Models
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : Microsoft.AspNetCore.Identity.IdentityUser
     {
-       public string? FirstName { get; set; }
+        public string? FirstName { get; set; }
         public string? LastName { get; set; }
     }
 }
+
